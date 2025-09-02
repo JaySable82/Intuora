1c027afb3dcf7b6bd5874e8d901752c19b95cb4b
rgvar25 <vardhamaneronit@gmail.com>
2025-09-02 11:24:54 +0530

    deleted backend folder

--- backend/Routes/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  mobileNumber: { type: String, required: true, unique: true },
-  password:    { type: String, required: true }, // store bcrypt hash
-});
-
-module.exports = mongoose.model('User', UserSchema);
--- backend/Routes/models/kitchenStatus.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const KitchenStatusSchema=new mongoose.Schema({
-    kitchenActive:Boolean
-});
-
-const KitchenStatusModel=mongoose.model('KitchenStatus',KitchenStatusSchema);
-
-export default KitchenStatusModel;
